942d6055a8a13b4a04d08c4b16db95f2e5972f69
Pascal Burkhard <pascal.burkhard@gmail.com>
2025-05-29 15:39:14 +0200

    fix: allow bind options

--- R/run_app.R
+++ R/run_app.R
@@ -1,21 +1,28 @@
 #' Run the Shiny Application
-#' 
+#'
 #' @param ... golem options
 #'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function(...) {
+run_app <- function(
+  onStart = NULL,
+  options = list(),
+  enableBookmarking = NULL,
+  uiPattern = "/",
+  ...
+) {
   cachedir <- rappdirs::user_cache_dir("geoapp")
   if (!file.exists(cachedir)) dir.create(cachedir, recursive = TRUE)
+
   with_golem_options(
     app = shinyApp(
       ui = app_ui,
       server = app_server,
-      options = list(
-        host = "0.0.0.0",
-        port = 3801
-      )
+      onStart = onStart,
+      options = options,
+      enableBookmarking = enableBookmarking,
+      uiPattern = uiPattern
     ),
     golem_opts = list(...)
   )
